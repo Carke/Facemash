98e4f88126adefbaaf37806cfb7651c0e1cc98da
Caleb Alexander <calexan@umich.edu>
2011-11-14 13:27:28 -0500

    main menu and integrate files

--- Source/main.py
+++ Source/main.py
@@ -5,6 +5,8 @@
 #
 
 import pygame, cv
+from camera import *
+from pygame.locals import *
 # import settings
 # import play
 # import learn
@@ -16,22 +18,38 @@ class Game():
 		screen = pygame.display.set_mode(pygame.FULLSCREEN)
 		pygame.display.set_caption('Facemash')
 	def event(self, event):
-		if event.type == QUIT:
+		if event.type == pygame.QUIT:
 			sys.exit()
-		elif event.type == KEYDOWN:
-			if event.key == K_ESCAPE:
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
 				sys.exit()
 
 def main():
-	game = Game()
-	clock = pygame.time.Clock()
-	running = True
-	while running:
-		clock.tick(60)
+	setting = 0
+	pygame.init()
+	screen = pygame.display.set_mode((640,480))
+	image = Camera()
+	expression = Expression()
+	while setting == 0:
+		expression.choose()
+		is_correct = False
+		lobster = pygame.font.match_font('Lobster 1.4,')
+		font = pygame.font.Font(lobster, 36)
+		text = font.render(expression.expression + " on the " + expression.side, 1, (255,255,255))
+		textpos = text.get_rect()
+		textpos = text.get_rect(centerx=screen.get_width()/2)
+		while not is_correct:
+			is_correct = image.detectExpression(expression)
+			#print expression.score
+			image.render(text, textpos)
+			pygame.display.flip()
+		event = pygame.event.poll()
 		pygame.display.flip()
-		for event in pygame.event.get():
-			game.event(event)
+		if event.type == KEYDOWN and event.key == K_ESCAPE:
+			setting = 1
+
 
 # Run the main program now.			
-main()
+if __name__ == "__main__":
+	main()
 
